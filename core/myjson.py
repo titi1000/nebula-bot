eb92ad3004dae3fd0425f67becc9ddef7ad242ce
voXrey <72698969+voXrey@users.noreply.github.com>
2021-06-17 19:40:08 +0200

    Update myjson.py
    - Add lang_json variable
    - Fix the encoding in utf-8

--- core/myjson.py
+++ core/myjson.py
@@ -7,9 +7,12 @@ class MyJson:
     
     # open the json file
     def open_json(self):
-        with open (self.json_path, 'r') as json_file:
+        with open (self.json_path, 'r', encoding='utf-8') as json_file:
             return json.load(json_file)
 
     # dump json dict
     def dump(self, json_dict:dict):
-        json.dump(json_dict, open(self.json_path,'w'), indent=4)
+        json.dump(json_dict, open(self.json_path,'w', encoding='utf-8'), indent=4)
+        
+lang_path = "lang.json"
+lang_json = MyJson(lang_path)
