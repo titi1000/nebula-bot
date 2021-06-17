17ffbc27def22b91a9a646a78b14d1593957b68d
voXrey <72698969+voXrey@users.noreply.github.com>
2021-06-17 18:22:54 +0200

    Add files via upload

---
+++ core/myjson.py
@@ -0,0 +1,15 @@
+import json
+
+class MyJson:
+    
+    def __init__(self, json_path):
+        self.json_path = json_path
+    
+    # open the json file
+    def open_json(self):
+        with open (self.json_path, 'r') as json_file:
+            return json.load(json_file)
+
+    # dump json dict
+    def dump(self, json_dict:dict):
+        json.dump(json_dict, open(self.json_path,'w'), indent=4)
