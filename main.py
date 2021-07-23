25c09420e4d699b5b948ba7bdd0715c724a3449a
titi1000 <74377782+titi1000@users.noreply.github.com>
2021-07-23 15:44:59 +0200

    minor changes

--- main.py
+++ main.py
@@ -200,11 +200,9 @@ class Bot(commands.Bot):
 
     async def on_guild_join(self, guild):
         db_punishments.add_guild(guild.id)
-        db.cursor.execute("SELECT `guild_id` FROM guilds WHERE `guild_id` = %s", (guild.id,))
-        result = db.cursor.fetchone()
-        if result is None:
-            db.cursor.execute("INSERT INTO guilds(`guild_id`) VALUES (%s)", (guild.id,))
-            db.commit()
+        result = db.db_fetchone("SELECT `guild_id` FROM guilds WHERE `guild_id` = %s", (guild.id,)) 
+        if result[1] is None:
+            db.db_execute("INSERT INTO guilds(`guild_id`) VALUES (%s)", (guild.id,))
 
         join_e = discord.Embed(
             title=f"Thanks for adding {self.user.name}!",
@@ -216,11 +214,9 @@ class Bot(commands.Bot):
 
     async def on_guild_remove(self, guild):
         db_punishments.remove_guild(guild.id)
-        db.cursor.execute("SELECT `guild_id` FROM guilds WHERE `guild_id` = %s", (guild.id,))
-        result = db.cursor.fetchone()
-        if result is not None:
-            db.cursor.execute("DELETE FROM guilds WHERE `guild_id` = %s", (guild.id,))
-            db.commit()
+        result = db.db_fetchone("SELECT `guild_id` FROM guilds WHERE `guild_id` = %s", (guild.id,))
+        if result[1] is not None:
+            db.db_execute("DELETE FROM guilds WHERE `guild_id` = %s", (guild.id,))
 
 
 # main function
@@ -230,4 +226,5 @@ def main():
 
         
 # run the bot
-if __name__ == "__main__": main()
+if __name__ == "__main__": 
+    main()
